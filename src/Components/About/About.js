448b90d629effe8881bbce341d9e429ca9e00f4f
kpandian <kpandian@ucsc.edu>
2020-07-31 12:54:40 -0700

    Mostly finished

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,7 @@ import "./About.scss";
 import Title from "../Shared/Title/Title.js";
 import Fade from 'react-reveal/Fade';
 import img from '../../Assets/Profile.JPG';
-
+import Card from 'react-bootstrap/Card'
 const About = () => {
     return (
         <React.Fragment>
@@ -11,6 +11,9 @@ const About = () => {
                 <Title title="About" />
                 <div class="profile">
                     <Fade down delay={300} distance={"0.5em"}>
+                    <div class="img">
+                            <img src={img} alt = "error"/>
+                        </div>
                         <div class="bio">
                             <div>
                                 Hello there! My name is Krishna Pandian, and I am a junior Computer Engineering Major at UC Santa Cruz. 
@@ -23,9 +26,6 @@ const About = () => {
                             <div>
                                 In my free time I love to work on side projects, play board games, and try new foods with friends.
                             </div>
-                        </div>
-                        <div class="img">
-                            <img src={img} alt = "error"/>
                         </div>
                     </Fade>
                 </div>
--- src/Components/Shared/imageIndex.js
+++ src/Components/Shared/imageIndex.js
@@ -3,7 +3,10 @@ const ImageIndex = (imageName) => {
         "CruzHacks_2020": require("../../Assets/CruzHacks_2020.JPG"),
         "Hackathons": require("../../Assets/Hackathons.JPG"),
         "SASE_WRC": require("../../Assets/SASE_WRC.jpeg"),
-        "Profile": require("../../Assets/Profile.JPG")
+        "Profile": require("../../Assets/Profile.JPG"),
+        "BSOE": require("../../Assets/BSOE.png"),
+        "Baskin": require("../../Assets/Baskin.jpg"),
+        "Tech4Good": require("../../Assets/T4g.png")
     }
     return images[imageName];
 }
