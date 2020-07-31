20b6f1db2a1c23239fcf12715c01b8738c25231e
kpandian <kpandian@ucsc.edu>
2020-07-31 12:57:32 -0700

    Updates

---
+++ precache-manifest.e4125a05f116ddb39c36bbfcbe9eb90f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7baa70338308c88d043547d122c08b5",
+    "url": "/krishna-pandian.github.io/index.html"
+  },
+  {
+    "revision": "383b74b5bc6038c152ec",
+    "url": "/krishna-pandian.github.io/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e08ddde4ac3a866c52e8",
+    "url": "/krishna-pandian.github.io/static/css/main.7d35e7ef.chunk.css"
+  },
+  {
+    "revision": "383b74b5bc6038c152ec",
+    "url": "/krishna-pandian.github.io/static/js/2.6c22b208.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/krishna-pandian.github.io/static/js/2.6c22b208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e08ddde4ac3a866c52e8",
+    "url": "/krishna-pandian.github.io/static/js/main.b22f2d1d.chunk.js"
+  },
+  {
+    "revision": "89b9e9765352d232cba5",
+    "url": "/krishna-pandian.github.io/static/js/runtime-main.2fec4f58.js"
+  },
+  {
+    "revision": "f32c8d7b6606afdbb2f24af1f8e1cab7",
+    "url": "/krishna-pandian.github.io/static/media/Baskin.f32c8d7b.jpg"
+  },
+  {
+    "revision": "10dc764eda74f513beba9766f5d22afe",
+    "url": "/krishna-pandian.github.io/static/media/CruzHacks_2020.10dc764e.JPG"
+  },
+  {
+    "revision": "cf3e2c56acef1ccaa5d6b7fb8ee1b58a",
+    "url": "/krishna-pandian.github.io/static/media/Hackathons.cf3e2c56.JPG"
+  },
+  {
+    "revision": "359d067813a80beb7fc479c4b33e3b07",
+    "url": "/krishna-pandian.github.io/static/media/Profile.359d0678.JPG"
+  },
+  {
+    "revision": "2aa4bdb5703568f8c0cb96f89e14f931",
+    "url": "/krishna-pandian.github.io/static/media/SASE_WRC.2aa4bdb5.jpeg"
+  },
+  {
+    "revision": "698a210cc896263943a5a1239997b358",
+    "url": "/krishna-pandian.github.io/static/media/T4g.698a210c.png"
+  },
+  {
+    "revision": "9a786e1efd70a2377e4f4aa33722ba6d",
+    "url": "/krishna-pandian.github.io/static/media/Web_Resume.9a786e1e.pdf"
+  },
+  {
+    "revision": "242e2f0b866fb8615345fe23f668cbcf",
+    "url": "/krishna-pandian.github.io/static/media/background.242e2f0b.jpg"
+  }
+]);
